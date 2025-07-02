51405e50d68ed47c8c5ec3474e6c1c521cc1216d
Med-azaza <hamaaz666@gmail.com>
2025-07-02 09:49:09 +0100

    ts type fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  library,
+  type IconDefinition,
+} from "@fortawesome/fontawesome-svg-core";
 import * as brands from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-  ...Object.values(brands).filter((icon) => icon.prefix === "fab"),
+  ...(Object.values(brands) as IconDefinition[]).filter(
+    (icon) => icon.prefix === "fab"
+  ),
   fas
 );
 
